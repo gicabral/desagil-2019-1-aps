e06aa17cc47da77b3880f340783facb46db7aef7
gicabral <38368320+gicabral@users.noreply.github.com>
2019-04-15 23:56:51 -0300

    APS 3

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -2,25 +2,36 @@ package br.pro.hashi.ensino.desagil.aps.view;
 
 import br.pro.hashi.ensino.desagil.aps.model.Gate;
 import br.pro.hashi.ensino.desagil.aps.model.Switch;
+import br.pro.hashi.ensino.desagil.aps.model.Light;
 
 import javax.swing.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
+import java.net.URL;
+import java.awt.*;
 
-public class GateView extends JPanel implements ItemListener {
+
+public class GateView extends FixedPanel implements  ActionListener, MouseListener {
     private final Switch[] switches;
     private final Gate gate;
 
     private final JCheckBox[] inputBoxes;
     private final JCheckBox outputBox;
+    private final Image images;
+    private Light light;
+
 
     public GateView(Gate gate) {
+        super(300, 270);
         this.gate = gate;
+        this.light = new Light();
+
 
         int inputSize = gate.getInputSize();
 
         switches = new Switch[inputSize];
         inputBoxes = new JCheckBox[inputSize];
+        light.connect(0, gate);
+        light.setR(255);
 
         for (int i = 0; i < inputSize; i++) {
             switches[i] = new Switch();
@@ -33,12 +44,17 @@ public class GateView extends JPanel implements ItemListener {
 
         JLabel inputLabel = new JLabel("Input");
         JLabel outputLabel = new JLabel("Output");
-
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
         add(inputLabel);
+
+        int c = -1;
         for (JCheckBox inputBox : inputBoxes) {
-            add(inputBox);
+            c +=1;
+            if(inputSize ==1){
+                add(inputBox, 30, 60, 25, 25);
+            }
+            else {
+                add(inputBox, 30, 120, 25, 25);
+            }
         }
         add(outputLabel);
         add(outputBox);
@@ -46,6 +62,13 @@ public class GateView extends JPanel implements ItemListener {
         for (JCheckBox inputBox : inputBoxes) {
             inputBox.addItemListener(this);
         }
+
+        String name = gate.toString() + ".png";
+        URL url = getClass().getClassLoader().getResource(name);
+        images = getToolkit().getImage(url);
+        addMouseListener(this);
+        update();
+
 
         outputBox.setEnabled(false);
 
@@ -61,13 +84,53 @@ public class GateView extends JPanel implements ItemListener {
             }
         }
 
-        boolean result = gate.read();
-
-        outputBox.setSelected(result);
+        repaint();
     }
 
     @Override
-    public void itemStateChanged(ItemEvent event) {
+    public void mouseClicked(MouseEvent event) {
+
+        int x = event.getX();
+        int y = event.getY();
+        Color color;
+
+        if (Math.pow((x-180),2) + Math.pow((y-57),2) <= Math.pow(20,2)){
+
+            color = JColorChooser.showDialog(this, null, new Color(light.getR(), light.getG(), light.getB()));
+            if (color != null) {
+                light.setB(color.getBlue());
+                light.setG(color.getGreen());
+                light.setR(color.getRed());
+            }
+            repaint();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {
         update();
     }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent event) {
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(images, 40, 40, 150, 70, this);
+        g.setColor(new Color(light.getR(), light.getG(), light.getB()));
+        g.fillOval(180, 50, 20, 25);
+        getToolkit().sync();
+    }
+
+
 }
